be443aa155a83b316cf6b5afe4a4574c00f69d14
John Hermes <hermesj@nwlschools.org>
2020-11-16 15:20:33 -0500

    fix arch from uname

--- src/freebsd-build.sh
+++ src/freebsd-build.sh
@@ -1,7 +1,7 @@
 rm -rf gamadv-xtd3
 rm -rf build
 rm -rf dist
-rm -rf gamadv-xtd3-$1-freebsd-$(arch).tar.xz
+rm -rf gamadv-xtd3-$1-freebsd-$(uname -p).tar.xz
 
 export LD_LIBRARY_PATH=/usr/local/lib
 pyinstaller --clean --noupx --strip -F --distpath=gamadv-xtd3 gam.spec
@@ -10,4 +10,4 @@ cp license.rtf gamadv-xtd3
 cp Gam*.txt gamadv-xtd3
 cp cacerts.pem gamadv-xtd3
 
-tar cfJ gamadv-xtd3-$1-freebsd-$(arch).tar.xz gamadv-xtd3/ 
+tar cfJ gamadv-xtd3-$1-freebsd-$(uname -p).tar.xz gamadv-xtd3/ 
